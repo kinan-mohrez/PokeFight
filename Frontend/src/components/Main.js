2010436553de6ddee780e6cd4e0359be073b5686
Kinan Mohrez <kinanmohrez2015@gmail.com>
2022-08-09 17:20:52 +0200

    tiny image next to pokemon name

--- Frontend/src/components/Main.js
+++ Frontend/src/components/Main.js
@@ -1,57 +1,57 @@
-import React from "react";
-import "./components.css";
-import { Routes, Route } from "react-router-dom";
-import About from "../components/About";
-import PokeFight from "./PokeFight";
-import Home from "../components/Home";
-import NotFound from "./NotFound";
-import PokeDetails from "../components/PokeDetails";
-import { useEffect, useState } from "react";
-import BaseInfo from "./BaseInfo";
+import React from 'react';
+import './components.css';
+import { Routes, Route } from 'react-router-dom';
+import About from '../components/About';
+import PokeFight from './PokeFight';
+import Home from '../components/Home';
+import NotFound from './NotFound';
+import PokeDetails from '../components/PokeDetails';
+import { useEffect, useState } from 'react';
+import BaseInfo from './BaseInfo';
 
 function Main() {
-  const [pokemons, setPokemons] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
+	const [pokemons, setPokemons] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
 
-  const fetchData = () => {
-    fetch("https://pokemon-fight-newproject.herokuapp.com/pokemons")
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        setIsLoading(false);
-        setPokemons(data);
-      });
-  };
+	const fetchData = () => {
+		fetch('https://pokemon-fight-newproject.herokuapp.com/pokemons')
+			.then((response) => {
+				return response.json();
+			})
+			.then((data) => {
+				setIsLoading(false);
+				setPokemons(data);
+			});
+	};
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+	useEffect(() => {
+		fetchData();
+	}, []);
 
-  return (
-    <div className="container">
-      <div className="main">
-        <Routes>
-          <Route
-            exact
-            path="/"
-            element={<Home pokemons={pokemons} isLoading={isLoading} />}
-          />
-          <Route path="/about" element={<About />} />
-          <Route path="/pokeFight" element={<PokeFight />} />
-          <Route
-            path="/:id"
-            element={<PokeDetails pokemons={pokemons} isLoading={isLoading} />}
-          />
-          <Route
-            path="/:id/:info"
-            element={<BaseInfo pokemons={pokemons} isLoading={isLoading} />}
-          />
-          <Route path="*" element={<NotFound />}></Route>
-        </Routes>
-      </div>
-    </div>
-  );
+	return (
+		<div className='container'>
+			<div className='main'>
+				<Routes>
+					<Route
+						exact
+						path='/'
+						element={<Home pokemons={pokemons} isLoading={isLoading} />}
+					/>
+					<Route path='/about' element={<About />} />
+					<Route path='/pokeFight' element={<PokeFight />} />
+					<Route
+						path='/:id'
+						element={<PokeDetails pokemons={pokemons} isLoading={isLoading} />}
+					/>
+					<Route
+						path='/:id/:info'
+						element={<BaseInfo pokemons={pokemons} isLoading={isLoading} />}
+					/>
+					<Route path='*' element={<NotFound />}></Route>
+				</Routes>
+			</div>
+		</div>
+	);
 }
 
 export default Main;
